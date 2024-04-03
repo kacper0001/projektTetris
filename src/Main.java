3d7d84f2a34d259fd3ac7abd238b1c996b866303
Rysia123 <agnieszkatumidajewicz2811@gmail.com>
2024-04-03 10:18:02 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane root = new GridPane();
+        Scene scene = new Scene(root);
+
+    }
+}
