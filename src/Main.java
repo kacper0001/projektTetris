17db9032c107c587b8ec530f7e041251209c310b
Rysia123 <agnieszkatumidajewicz2811@gmail.com>
2024-04-03 10:18:02 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,22 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+     public static int HEIGHT =50;
+     public static int WIDTH = 50;
+
+
+
     public static void main(String[] args) {
-
-
+        launch(args);
     }
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         GridPane root = new GridPane();
         Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
