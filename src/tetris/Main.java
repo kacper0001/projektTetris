6794ff13de995fac8161db0f192c28c5cb7d9c61
Rysia123 <agnieszkatumidajewicz2811@gmail.com>
2024-04-05 20:52:17 +0200

    innit

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -9,9 +9,11 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class Main extends Application {
-     public static int HEIGHT =500;
+     public static int HEIGHT =1000;
      public static int WIDTH = 500;
      public static int SIZE = 20;
+    public static GridPane ROOT = new GridPane();
+    public static Rectangle [][] RECTANGLES = new Rectangle[HEIGHT/SIZE][WIDTH/SIZE];
 
 
 
@@ -22,18 +24,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane root = new GridPane();
-        Scene scene = new Scene(root,WIDTH,HEIGHT);
-        Rectangle [][] rectangles = new Rectangle[HEIGHT/SIZE][WIDTH/SIZE];
+        Scene scene = new Scene(ROOT,WIDTH,HEIGHT);
+
 
         for (int r = 0; r <HEIGHT/SIZE ; r++) {
             for (int c = 0; c <WIDTH/SIZE; c++) {
                 Rectangle rectangle = new Rectangle(SIZE,SIZE, Color.BLACK);
-                rectangles[r][c] = rectangle;
-                root.add(rectangle, c*SIZE, r*SIZE);
+                RECTANGLES[r][c] = rectangle;
+                ROOT.add(rectangle, c*SIZE, r*SIZE);
             }
 
         }
+
+        Rectangle middle = RECTANGLES[2][RECTANGLES[0].length/2];// sprawdzenie
+        Color current = Piece.chooseColor();
+        middle .setFill(current);
+        Piece.create(middle,current,5);
+
+
         primaryStage.setScene(scene);
         primaryStage.show();
 
--- src/tetris/Piece.java
+++ src/tetris/Piece.java
@@ -7,48 +7,66 @@ import static tetris.Main.*;
 
 public class Piece extends Rectangle {
 
-    public static void create( Rectangle middle, Color current){
-        int count = 5;
-        middle.setX(WIDTH/2);
-        middle.setY(HEIGHT - 3*SIZE);
-        while(count>=0){
-            for (int r = HEIGHT - 3*SIZE ; r < HEIGHT / SIZE; r++) {
-                for (int c = WIDTH/2; c < WIDTH/SIZE ; c++) {
-                    double random = Math.random();
-                    if(random < 0.25){
-                        Rectangle rectangle = new Rectangle( WIDTH/2 - SIZE, HEIGHT - 3*SIZE, SIZE,SIZE );
-                        rectangle.setFill(current);
-                        create(rectangle, current);
-                        count--;
 
 
+    public static void create(Rectangle middle, Color current, int count) { // tworzy piece (count to ilość kwadratów która ma być w jednym piece)
+        while (count >0) {
+        for (int r =0; r < HEIGHT/SIZE; r ++) {
+                for (int c = 0; c < WIDTH/SIZE; c ++) {
+                    if(RECTANGLES[r][c] == middle) {
+                        int random = (int) (Math.random() *4);
 
+                        if (random == 1  && r - 1 >=0 ) {
+                            RECTANGLES[r-1][c].setFill(current);
+                            middle = RECTANGLES[r-1][c];
+                            create(middle, current,count--);
+                        }
+                        else if (random == 2 && r + 1 < HEIGHT/SIZE) {
+                            RECTANGLES[r+1][c].setFill(current);
+                            middle = RECTANGLES[r+1][c];
+                            create(middle, current,count--);
+
+
+                        }
+                        else if (random ==3 && c +1 < WIDTH/SIZE) {
+                            RECTANGLES[r][c+1].setFill(current);
+                            middle = RECTANGLES[r][c+1];
+                            create(middle, current,count--);
+
+                        }
+                        else if (random ==4 && c - 1 > 0) {
+                            RECTANGLES[r][c-1].setFill(current);
+                            middle = RECTANGLES[r][c-1];
+                            create(middle, current,count--);
+
+                        }
+                        else {
+                            create(middle, current,count--);
+
+                        }
                     }
-                    
                 }
+
+
             }
-            
-            
         }
     }
 
-    private static Color ChooseColor() {
-        double random = Math.random();
-        Color color = Color.GREEN;
-        if(random<= 0.2){
-            color = Color.YELLOW;
+        static Color chooseColor() { //wybiera kolor
+            double random = Math.random();
+            Color color = Color.GREEN;
+            if (random <= 0.2) {
+                color = Color.YELLOW;
+            } else if (random > 0.2 && random <= 0.4) {
+                color = Color.BLUE;
+            } else if (random > 0.4 && random <= 0.6) {
+                color = Color.RED;
+            } else if (random > 0.6 && random <= 0.8) {
+                color = Color.PINK;
+            }
+
+            return color;
         }
-        else if(random> 0.2 && random<=0.4){
-            color = Color.BLUE;
-        }
-        else if(random> 0.4 && random<=0.6){
-            color = Color.RED;
-        }
-        else if(random> 0.6 && random<=0.8){
-            color = Color.PINK;
-        }
-        
-        return color;
     }
 
-}
+
